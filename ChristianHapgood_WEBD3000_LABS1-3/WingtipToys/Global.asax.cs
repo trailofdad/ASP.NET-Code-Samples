e48764feb642e64855706f9a0ff94364b6369931
Christian Hapgood <w0278262@nscc.ca>
2016-02-09 14:58:26 -0400

    Added error handling and ELMAH

--- ChristianHapgood_WEBD3000_LABS1-3/WingtipToys/Global.asax.cs
+++ ChristianHapgood_WEBD3000_LABS1-3/WingtipToys/Global.asax.cs
@@ -44,6 +44,24 @@ namespace WingtipToys
                 "~/ProductDetails.aspx"
             );
         }
+        void Application_Error(object sender, EventArgs e)
+        {
+            // Code that runs when an unhandled error occurs.
+
+            // Get last error from the server
+            Exception exc = Server.GetLastError();
+
+            if (exc is HttpUnhandledException)
+            {
+                if (exc.InnerException != null)
+                {
+                    exc = new Exception(exc.InnerException.Message);
+                    Server.Transfer("ErrorPage.aspx?handler=Application_Error%20-%20Global.asax",
+                        true);
+                }
+            }
+        }
     }
     }
+
 // removed an extra brace
