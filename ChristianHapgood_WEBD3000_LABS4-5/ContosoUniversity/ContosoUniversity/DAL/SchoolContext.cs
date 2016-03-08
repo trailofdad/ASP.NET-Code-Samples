c3bcb0b7f2ac7ded835f40c19bc9327eda222b51
Christian Hapgood <w0278262@nscc.ca>
2016-03-08 10:19:19 -0400

    Handling concurrency finished.

--- ChristianHapgood_WEBD3000_LABS4-5/ContosoUniversity/ContosoUniversity/DAL/SchoolContext.cs
+++ ChristianHapgood_WEBD3000_LABS4-5/ContosoUniversity/ContosoUniversity/DAL/SchoolContext.cs
@@ -24,6 +24,7 @@ namespace ContosoUniversity.DAL
                 .Map(t => t.MapLeftKey("CourseID")
                     .MapRightKey("InstructorID")
                     .ToTable("CourseInstructor"));
+           modelBuilder.Entity<Department>().MapToStoredProcedures();
 
 
         }
